17b0df824aaaf0fc803ee2c7e13d09d04d079daf
Divyaminevitable <110938873+Divyaminevitable@users.noreply.github.com>
2025-10-09 09:19:27 +0000

    variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,15 @@
+const account_Id = 14453
+let accountEmail = "divyams@gmail.com" 
+var accountPassword = "12654"
+accountCity = "Jaipur"
+let accountstate;
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+console.log(account_Id);
+ 
+console.table([account_Id, accountEmail, accountPassword, accountCity, accountstate])
+
+// Prefer not to use var 
+// because of issue in block scope and functional scope
