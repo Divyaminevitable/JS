434b9282d817d596e5b281973c18907ced6aba0f
Divyaminevitable <110938873+Divyaminevitable@users.noreply.github.com>
2025-10-25 11:31:34 +0000

    This & arrow function

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -3,9 +3,42 @@ const user ={
     price: 99,
 
 welcomeMessage: function() {
+    console.log(this);
+    
     console.log(` ${this.username}, welcome to website`);
 }
 }
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "divyam"
+//     console.log(this.username);   
+// }
+
+// const chai = () => {                   }
+//     let username = "divyam"            }--- basic arrow function
+//     console.log(this);                 }
+// }
+// chai()
+
+//              Implicit return
+// if u use curly braces then u have to write return but in parenthesis() or without the bracket u don't have to write return in arrow function
+
+// const addTwo = (num1, num2) => num1 + num2
+
+const addTwo = (num1, num2) => {username: "divyam"}
+// const addTwo = (num1, num2) => ({username: "divyam"}) 
+// object ko return krne ke liye parethesis () mei wrap krna hi hoga
+
+console.log(addTwo(3,4));
